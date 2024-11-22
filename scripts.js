24f217c48dbd6d7fc5660173ac3f8f0efb333065
JexSparrow <jexxsantos@hotmail.com>
2024-11-21 23:28:26 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,51 @@
+const background = document.querySelector('.polygon')
+const celular = document.querySelector('.iphone')
+const header = document.querySelector('#header')
+const botao = document.querySelector('button')
+const h1 = document.querySelector('#h1')
+
+const red = document.querySelector('#vermelho')
+const blue = document.querySelector('#azul')
+const yellow = document.querySelector('#amarelo')
+const black = document.querySelector('#preto')
+const white = document.querySelector('#branco')
+const purple = document.querySelector('#roxo')
+
+
+function mudarCor(cor){
+
+    background.style.background = cor
+    botao.style.background = cor
+    h1.style.color = cor
+    header.style.borderColor = cor
+    
+    
+
+}
+
+function trocarImagem(img){
+    celular.src = img
+    
+
+}
+
+function trocarBoxShadow(cor){
+
+    
+
+    botao.style.backgroundColor = cor
+    celular.style.maxWidth = '31%'
+    celular.style.transform = 'rotateY(180deg)'
+    header.style.boxShadow = '2px 2px 10px #32393F'
+    header.style.borderColor = cor
+
+
+}
+
+function mouseD(){
+    celular.style.maxWidth = '30%'
+    celular.style.transform = 'rotateY(0deg)'
+    header.style.boxShadow = '1px 1px 10px #DBD7D4'
+    
+
+}
